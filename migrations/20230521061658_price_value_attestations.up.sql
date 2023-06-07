c906eb7d40f428e4239a3bf451f4a1e4a7c04cce
test <test@gmail.com>
2023-06-07 14:53:48 +0800

    Fix casing in migration files

--- migrations/20230521061658_price_value_attestations.up.sql
+++ migrations/20230521061658_price_value_attestations.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE price_value_attestations (
-    validator_public_key text NOT NULL,
+    validator_public_key TEXT NOT NULL,
     slot_number BIGINT NOT NULL,
     value BIGINT NOT NULL,
-    signature text NOT NULL,
+    signature TEXT NOT NULL,
     PRIMARY KEY (validator_public_key, slot_number)
 );
 
--- migrations/20230521105847_price_interval_attestations.up.sql
+++ migrations/20230521105847_price_interval_attestations.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE price_interval_attestations (
-    validator_public_key text NOT NULL,
-    signature text NOT NULL PRIMARY KEY,
+    validator_public_key TEXT NOT NULL,
+    signature TEXT NOT NULL PRIMARY KEY,
     slot_number  BIGINT NOT NULL,
     value  BIGINT NOT NULL,
     interval_size  BIGINT NOT NULL
